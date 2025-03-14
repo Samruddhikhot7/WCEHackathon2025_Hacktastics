689ed55441a357532c0d255bcfa2a0f080e0b424
Atharva-Tangadi <a.s.tangadi07@gmail.com>
2025-03-14 22:46:57 +0530

    Create notifiction_for_student.dart

---
+++ lib/notifiction_for_student.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class NotifictionForStudent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Notification for Students'),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {
+              // Handle notification action
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Menu', style: TextStyle(color: Colors.white, fontSize: 24)),
+              decoration: BoxDecoration(
+                color: Colors.purple,
+              ),
+            ),
+            ListTile(
+              title: Text('About Us'),
+              onTap: () {
+                // Handle About Us action
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Settings'),
+              onTap: () {
+                // Handle Log Out action
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Log Out'),
+              onTap: () {
+                // Handle Log Out action
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(child: Text('mess biling Page')),
+    );
+  }
+}
