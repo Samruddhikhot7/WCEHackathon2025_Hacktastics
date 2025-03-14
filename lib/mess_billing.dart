ea1740ff1b77cd8f78f22be2d4e5dc5740bdb02d
Atharva-Tangadi <a.s.tangadi07@gmail.com>
2025-03-14 22:39:57 +0530

    Create mess_billing.dart

---
+++ lib/mess_billing.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import 'notification_for_mess.dart';
+
+class MessBilling extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('mess biling'),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => NotificationForMess()),
+              );
+              // Handle notification action
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Menu', style: TextStyle(color: Colors.white, fontSize: 24)),
+              decoration: BoxDecoration(
+                color: Colors.purple,
+              ),
+            ),
+            ListTile(
+              title: Text('About Us'),
+              onTap: () {
+                // Handle About Us action
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Settings'),
+              onTap: () {
+                // Handle Log Out action
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Log Out'),
+              onTap: () {
+                // Handle Log Out action
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(child: Text('mess biling Page')),
+    );
+  }
+}
