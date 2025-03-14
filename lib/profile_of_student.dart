355c5b0b662935e7f67f6fea292504bb5f80e0f7
Sai Mandar Badave <151989300+SaiBadave@users.noreply.github.com>
2025-03-14 22:50:53 +0530

    Create profile_of_student.dart

---
+++ lib/profile_of_student.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import 'notifiction_for_student.dart';
+
+class ProfileOfStudent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('profile of student'),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => NotifictionForStudent()),
+              );
+              // Handle notification action
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Menu', style: TextStyle(color: Colors.white, fontSize: 24)),
+              decoration: BoxDecoration(
+                color: Colors.purple,
+              ),
+            ),
+            ListTile(
+              title: Text('About Us'),
+              onTap: () {
+                // Handle About Us action
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Settings'),
+              onTap: () {
+                // Handle Log Out action
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Log Out'),
+              onTap: () {
+                // Handle Log Out action
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(child: Text('mess biling Page')),
+    );
+  }
+}
